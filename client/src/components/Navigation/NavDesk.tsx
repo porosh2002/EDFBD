781e4d6af921901ab64326f7f735afd0cf013aa1
porosh2002 <jamilkashem2002@gmail.com>
2020-12-26 22:06:37 +0600

    Navigation component updated

---
+++ client/src/components/Navigation/NavDesk.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+    render() {
+        console.log('Navigation_Desk')
+        return (
+            <div>
+               <h1>Desktop </h1>
+            </div>
+        )
+    }
+}
--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
-
+import React, { Component, Suspense } from "react";
+const NavDesk = React.lazy(() => import("./NavDesk"));
+const NavMob = React.lazy(() => import("./NavMob"));
 export default class Navigation extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+    render() {  
+    return (
+      <div>
+        {window.innerWidth > 900 ? (
+          <Suspense fallback={<div>Loading...</div>}>
+            <NavDesk />
+          </Suspense>
+        ) : (
+          <Suspense fallback={<div>Loading...</div>}>
+            <NavMob />
+          </Suspense>
+        )}
+      </div>
+    );
+  }
 }
