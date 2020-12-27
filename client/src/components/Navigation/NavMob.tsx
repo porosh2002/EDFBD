8a3c4455f975121b6033e5bce1f95fb8d049657c
porosh2002 <jamilkashem2002@gmail.com>
2020-12-28 00:47:53 +0600

    nav Mob Structure

--- client/src/components/Navigation/NavMob.tsx
+++ client/src/components/Navigation/NavMob.tsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-
+import { Nav,LinkC } from '../../Styled/Styled';
+import LogoImage from '../../images/Logo.png';
+import { AiOutlineMenu } from 'react-icons/ai';
 export default class Navigation extends Component {
     render() {
         return (
-            <div>
-                Mobile
-            </div>
+            <Nav className='NavMobile'>
+                <LinkC to='/' className='LogoLink'>
+                <img className="Logo" src={LogoImage} alt='EDFBD'></img>
+                </LinkC>
+                <div className='Bars'>
+                <AiOutlineMenu />
+                </div>
+            </Nav>
         )
     }
 }
