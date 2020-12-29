8d79769b1f40fd34ae50ad075ae91ab512f9f4f8
porosh2002 <jamilkashem2002@gmail.com>
2020-12-30 03:39:33 +0600

    Slider added

--- client/src/components/Slider/Slider.tsx
+++ client/src/components/Slider/Slider.tsx
@@ -1,31 +1,68 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Content } from "../../Styled/Styled";
-import SwiperCore, { EffectFade } from "swiper";
+import SwiperCore, { EffectFade,Autoplay } from "swiper";
 import "swiper/swiper.scss";
-SwiperCore.use([EffectFade]);
+import { Link } from "react-router-dom";
+SwiperCore.use([EffectFade,Autoplay]);
 export default function Slider() {
   return (
     <div className='Slider_padding'>
       <Swiper
         effect="fade"
         slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        loop={true}
+        autoplay={{
+          delay: 3000
+      }}
       >
         <SwiperSlide>
-          <div className="Slider">
+          <div className="Slider Slider0">
             <div className="Slider_Div">
-              <Content>
-
+              <Content className='ContentSlider'>
+                <p className='SliderTxtTitle'>
+                  Welcome to EDF. EDF is your most dedicated partner
+                </p>
+                <Link className='SliderCTA' to='#'>Donate Now</Link>
               </Content>
             </div>
           </div>
         </SwiperSlide>
-        {/* <SwiperSlide>
-        <div className="Slider">
-      <div className="Slider_Div"></div>
-    </div>
-      </SwiperSlide> */}
+        <SwiperSlide>
+          <div className="Slider Slider3">
+            <div className="Slider_Div">
+              <Content className='ContentSlider'>
+                <p className='SliderTxtTitle'>
+                EDF Is A New Born Idea, A Call Of Unity.
+                </p>
+                <Link className='SliderCTA' to='#'>Donate Now</Link>
+              </Content>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="Slider Slider2">
+            <div className="Slider_Div">
+              <Content className='ContentSlider'>
+                <p className='SliderTxtTitle'>
+                Change YourSelf And Change the World.
+                </p>
+                <Link className='SliderCTA' to='#'>Donate Now</Link>
+              </Content>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="Slider Slider1">
+            <div className="Slider_Div">
+              <Content className='ContentSlider'>
+                <p className='SliderTxtTitle'>
+                EDF is the open heart Kingdom where you help yourself to shape your Digital Victory
+                </p>
+                <Link className='SliderCTA' to='#'>Donate Now</Link>
+              </Content>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
